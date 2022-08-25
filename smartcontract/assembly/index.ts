626f880e3e499e02bc2d73776524c157aceab8b6
Blisado1 <blessededison410b@gmail.com>
2022-08-25 18:23:22 +0100

    finished contract

--- smartcontract/assembly/index.ts
+++ smartcontract/assembly/index.ts
@@ -1,60 +1,266 @@
-import { Product, productsStorage } from './model';
-import { context, ContractPromiseBatch } from "near-sdk-as";
+import { checkIfInvestor, getContractParams, Investor, Investors, MinDeposit, Proposal, Proposals, updateContractParams } from "./model";
+import { context, ContractPromiseBatch, u128 } from "near-sdk-as";
 
 /**
  * 
  * This function changes the state of data in the blockchain. 
- * It is used to issue buy transactions when a product is purchased from a given seller (if the product is available)
+ * It is used to start the DAO
  * 
- * @param productId - an identifier of a product that is the subject of purchase
+ * @param daoLife - How long the DAO is going to run;
+ * @param voteTimeLimit - How long the investors have to vote
+ * @param quorum - Min number of people needed for a proposal to be executed on the DAO
  */
-export function buyProduct(productId: string): void {
-    const product = getProduct(productId);
-    if (product == null) {
-        throw new Error("product not found");
-    }
-    if (product.price.toString() != context.attachedDeposit.toString()) {
-        throw new Error("attached deposit should be greater than the product's price");
-    }
-    /*
-        `ContractPromiseBatch` is used here to create a transaction to transfer the money to the seller
-        The amount of money to be used in the transaction is taken from `context.attachedDeposit` 
-        which is defined by `--depositYocto=${AMOUNT}` parameter during the invocation 
-    */
-    ContractPromiseBatch.create(product.owner).transfer(context.attachedDeposit);
-    product.incrementSoldAmount();
-    productsStorage.set(product.id, product);
+export function startDAO(daoLife: u64, voteTimeLimit: u64, quorum: u32): void {
+    assert(context.predecessor == context.contractName, "Method is private");
+
+    // get Contract parameters from storage
+    const contractParameters = getContractParams();
+
+    // set the parameters
+    contractParameters.setContractParams(daoLife, voteTimeLimit, quorum);
+
+    // update the contract parameters in storage
+    updateContractParams(contractParameters);
 }
 
 /**
  * 
- * @param product - a product to be added to the blockchain
+ * This function changes the state of data in the blockchain. 
+ * It is used to contribute to the DAO, by adding funds and in return the user gets investor rights.
+ * 
  */
-export function setProduct(product: Product): void {
-    let storedProduct = productsStorage.get(product.id);
-    if (storedProduct !== null) {
-        throw new Error(`a product with id=${product.id} already exists`);
+export function contributeToDAO(): void {
+    const contractParameters = getContractParams();
+
+    assert(context.blockTimestamp < contractParameters.daoLife, "Dao is currently closed to contributions");
+
+    assert(u128.gt(context.attachedDeposit, MinDeposit) || u128.eq(context.attachedDeposit, MinDeposit), "Amount Must be greater or equal to 1 Near")
+
+    let investor: Investor;
+
+    if (checkIfInvestor(context.predecessor)) {
+        investor = getInvestor(context.predecessor);
+    } else {
+        investor = new Investor(context.predecessor);
     }
-    productsStorage.set(product.id, Product.fromPayload(product));
+
+    investor.updateShares(context.attachedDeposit, "add");
+
+    Investors.set(context.predecessor, investor);
+
+    // update available funds and total shares
+    contractParameters.availableFunds = u128.add(contractParameters.availableFunds, context.attachedDeposit);
+    contractParameters.totalShares = u128.add(contractParameters.totalShares, context.attachedDeposit);
+
+    updateContractParams(contractParameters);
+}
+
+
+/**
+ * 
+ * This function changes the state of data in the blockchain. 
+ * It is used to remove funds from dao. Investors convert their shares back to near
+ * 
+ */
+export function redeemShares(amountToRedeem: u128): void {
+    const contractParameters = getContractParams();
+
+    assert(checkIfInvestor(context.predecessor), "Only Investors are allowed to use this feature")
+
+    assert(u128.gt(contractParameters.availableFunds, amountToRedeem) || u128.eq(contractParameters.availableFunds, amountToRedeem), "not enough available funds, please try again later");
+
+    const investor = getInvestor(context.predecessor);
+
+    assert(u128.gt(investor.shares, amountToRedeem) || u128.eq(investor.shares, amountToRedeem), "not enough shares");
+
+    // update investor shares
+    investor.updateShares(amountToRedeem, "sub");
+
+    // check if investor still has shares else revoke investor rights
+    if (!investor.stillHasShares()) {
+        investor.revokeRights();
+        Investors.set(context.predecessor, null)
+    } else {
+        Investors.set(context.predecessor, investor);
+    }
+
+    //send money
+    ContractPromiseBatch.create(context.predecessor).transfer(amountToRedeem);
+
+    // update contract parameters
+    contractParameters.updateAvailableFunds(amountToRedeem, "sub");
+
+    contractParameters.updateShares(amountToRedeem, "sub");
+
+    // update values in storage
+    updateContractParams(contractParameters);
+}
+
+
+/**
+ * 
+ * This function changes the state of data in the blockchain. 
+ * It is used to remove funds from dao. Investors convert their shares back to near
+ * 
+ */
+export function transferShares(amountToTransfer: u128, to: string): void {
+
+    assert(checkIfInvestor(context.predecessor), "Only Investors are allowed to use this feature")
+
+    const investor = getInvestor(context.predecessor);
+
+    assert(u128.gt(investor.shares, amountToTransfer) || u128.eq(investor.shares, amountToTransfer), "not enough shares")
+
+    // update investor shares
+    investor.updateShares(amountToTransfer, "sub");
+
+    // check if investor still has shares else revoke investor rights
+    if (!investor.stillHasShares()) {
+        investor.revokeRights();
+        Investors.set(context.predecessor, null)
+    } else {
+        Investors.set(context.predecessor, investor);
+    }
+
+    // investor To
+    let investorTo: Investor;
+
+    if (checkIfInvestor(to)) {
+        investorTo = getInvestor(to);
+    } else {
+        investorTo = new Investor(to);
+    }
+
+    investorTo.updateShares(context.attachedDeposit, "add");
+
+    // update values in storage
+    Investors.set(context.predecessor, investorTo);
+}
+
+
+/**
+ * 
+ * @param proposal - a proposal to be added to the blockchain
+ */
+export function createProposal(proposal: Proposal): void {
+    const contractParameters = getContractParams();
+
+    assert(checkIfInvestor(context.predecessor), "Only Investors are allowed to use this feature")
+
+    let storedProposal = Proposals.get(proposal.id);
+
+    if (storedProposal !== null) {
+        throw new Error(`a proposal with id=${proposal.id} already exists`)
+    }
+
+    assert(u128.gt(contractParameters.availableFunds, proposal.amount) || u128.eq(contractParameters.availableFunds, proposal.amount), "amount too big");
+
+    Proposals.set(proposal.id, Proposal.fromPayload(proposal));
 }
 
 /**
  * 
- * A function that returns a single product for given owner and product id
- * 
- * @param id - an identifier of a product to be returned
- * @returns a product for a given @param id
+ * @param proposalId - an identifier of a proposal to be voted on
+ *
  */
-export function getProduct(id: string): Product | null {
-    return productsStorage.get(id);
+
+export function vote(proposalId: string): void {
+    assert(checkIfInvestor(context.predecessor), "Only Investors are allowed to use this feature")
+
+    let proposal = getProposal(proposalId);
+
+    assert(!proposal.checkVoter(context.predecessor), "Investor can only vote once for a proposal")
+
+    assert(context.blockTimestamp < proposal.ends, "Proposal voting period has ended");
+
+    let investor = getInvestor(context.predecessor)
+
+    proposal.addVote(investor.shares);
+
+    Proposals.set(proposalId, proposal);
+}
+
+
+/**
+ * 
+ * @param proposalId - an identifier of a proposal to be executed
+ *
+ */
+
+export function executeProposal(proposalId: string): void {
+    const contractParameters = getContractParams();
+
+    assert(contractParameters.admin == context.predecessor, "Only Admin is allowed to use this feature")
+
+    let proposal = getProposal(proposalId);
+
+    assert(context.blockTimestamp >= proposal.ends, "cannot execute proposal before end date")
+
+    assert(!proposal.executed, "cannot execute proposal already executed");
+
+    //calculate vote percentage
+    const votePercentage = u128.mul((u128.div(proposal.votes, contractParameters.totalShares)), u128.from(100));
+
+    assert(u128.gt(votePercentage, u128.from(contractParameters.quorum)) || u128.eq(votePercentage, u128.from(contractParameters.quorum)), "cannot execute proposal with votes # below quorum");
+
+    //transfer the funds and execute the proposal
+    assert(u128.gt(contractParameters.availableFunds, proposal.amount) || u128.eq(contractParameters.availableFunds, proposal.amount), "amount too big");
+
+    contractParameters.updateAvailableFunds(proposal.amount, "sub");
+
+    updateContractParams(contractParameters);
+}
+
+
+function getInvestor(accountId: string): Investor {
+    let investor = Investors.get(accountId);
+
+    if (investor == null) {
+        throw new Error(`${accountId} does not exist on record`)
+    }
+
+    return investor;
+}
+
+function getProposal(proposalId: string): Proposal {
+    let proposal = Proposals.get(proposalId);
+
+    if (proposal === null) {
+        throw new Error(`a proposal with id=${proposalId} does not exist`)
+    }
+
+    return proposal;
+
 }
 
 /**
  * 
  * A function that returns an array of products for all accounts
  * 
- * @returns an array of objects that represent a product
+ * @returns an array of objects that represent a proposal
  */
-export function getProducts(): Array<Product> {
-    return productsStorage.values();
+export function getProposals(): Array<Proposal> {
+    return Proposals.values();
+}
+
+
+export function getInvestorData(accountId: string): Investor | null {
+    let investor = Investors.get(accountId);
+
+    if (investor == null) {
+        return null;
+    }
+
+    return investor;
+}
+
+
+export function hasVoted(accountId: string, proposalId: string): bool {
+    let proposal = Proposals.get(proposalId);
+
+    if (proposal === null) {
+        return false
+    }
+
+    return proposal.checkVoter(accountId);
 }
--- smartcontract/assembly/model.ts
+++ smartcontract/assembly/model.ts
@@ -1,47 +1,152 @@
-import { PersistentUnorderedMap, context, PersistentMap, u128 } from "near-sdk-as";
+import { PersistentUnorderedMap, context, PersistentMap, storage, u128 } from "near-sdk-as";
 
 /**
- * This class represents a product that can be listed on a marketplace.
- * It contains basic properties that are needed to define a product.
- * The price of the product is of type u128 that allows storing it in yocto-NEAR, where `1 yocto = 1^-24`.
+ * This class represents the contract parameters.
+ * It contains basic properties that are needed to define the contract.
  * {@link nearBindgen} - it's a decorator that makes this class serializable so it can be persisted on the blockchain level. 
  */
 @nearBindgen
-export class Product {
-    id: string;
-    name: string;
-    description: string;
-    image: string;
-    location: string;
-    price: u128;
-    owner: string;
-    sold: u32;
-    public static fromPayload(payload: Product): Product {
-        const product = new Product();
-        product.id = payload.id;
-        product.name = payload.name;
-        product.description = payload.description;
-        product.image = payload.image;
-        product.location = payload.location;
-        product.price = payload.price;
-        product.owner = context.sender;
-        return product;
+export class contractParameters {
+    admin: string = "";
+    quorum: u32 = 0;
+    totalShares: u128 = initialValue;
+    voteTime: u64 = 0;
+    availableFunds: u128 = initialValue;
+    daoLife: u64 = 0;
+
+    public setContractParams(daoLife: u64, voteTimeLimit: u64, quorum: u32): void {
+        this.admin = context.predecessor;
+        this.daoLife = daoLife;
+        this.voteTime = voteTimeLimit;
+        this.quorum = quorum;
     }
-    public incrementSoldAmount(): void {
-        this.sold = this.sold + 1;
+
+    public updateShares(shares: u128, command: string): void {
+        if (command == "add") {
+            this.totalShares = u128.add(this.totalShares, shares);
+        } else if (command == "sub") {
+            this.totalShares = u128.sub(this.totalShares, shares);
+        }
     }
+
+    public updateAvailableFunds(funds: u128, command: string): void {
+        if (command == "add") {
+            this.availableFunds = u128.add(this.availableFunds, funds);
+        } else if (command == "sub") {
+            this.availableFunds = u128.sub(this.availableFunds, funds);
+        }
+    }
+
+    public updateAdmin(newAdmin: string): void {
+        this.admin = newAdmin;
+    }
+}
+export const initialValue: u128 = u128.from("000000000000000000000000");
+const contractInstance = new contractParameters();
+
+export function updateContractParams(contract: contractParameters): void {
+    storage.set<contractParameters>("contract", contract)
+}
+
+export function getContractParams(): contractParameters {
+    if (!storage.contains("contract")) { return contractInstance }
+    return storage.getSome<contractParameters>("contract")
 }
 
 /**
- * `productsStorage` - it's a key-value datastructure that is used to store products by sellers.
- * The backbone of this datastructure is {@link PersistentUnorderedMap} - a facade in front of the NEAR's {@link Storage}.
- * For the sake of this contract we've chosen {@link PersistentUnorderedMap} as a storage for the next reasons:
- * - `set`, `get` and `delete` operations have a constant time complexity - O(1)
- * - keys are stored in the blockchain (which is opposite to {@link PersistentMap})
- * - provides an interface to return a range of values
- * 
- * Brakedown of the `PersistentUnorderedMap<string, Product>` datastructure:
- * - the key of `PersistentUnorderedMap` is a `productId`
- * - the value in this `PersistentUnorderedMap` is a product itself `Product` that is related to a given key (`productId`)
+ * This class represents a proposal.
+ * It contains basic properties that are needed to define a proposal.
+ * {@link nearBindgen} - it's a decorator that makes this class serializable so it can be persisted on the blockchain level. 
  */
-export const productsStorage = new PersistentUnorderedMap<string, Product>("LISTED_PRODUCTS");
+@nearBindgen
+export class Proposal {
+    id: string;
+    name: string;
+    amount: u128;
+    recipient: string;
+    votes: u128;
+    ends: u64;
+    executed: bool;
+    private voters: Array<String>;
+
+    public static fromPayload(payload: Proposal): Proposal {
+        const proposal = new Proposal();
+        proposal.id = payload.id;
+        proposal.name = payload.name;
+        proposal.amount = payload.amount;
+        proposal.recipient = payload.recipient;
+        proposal.votes = initialValue;
+        proposal.ends = context.blockTimestamp;
+        proposal.executed = false;
+        proposal.voters = new Array<String>();
+        return proposal;
+    }
+
+    public addVote(shares: u128): void {
+        this.votes = u128.add(this.votes, shares);
+        this.voters.push(context.predecessor);
+    }
+
+    public executeProposal(): void {
+        this.executed = true;
+    }
+
+    // check if investor has voted
+    public checkVoter(accountId: string): bool {
+        return this.voters.includes(accountId);
+    }
+}
+
+export const Proposals = new PersistentUnorderedMap<string, Proposal>("PROPOSALS");
+
+/**
+ * This class represents the contract parameters.
+ * It contains basic properties that are needed to define the contract.
+ * {@link nearBindgen} - it's a decorator that makes this class serializable so it can be persisted on the blockchain level. 
+ */
+@nearBindgen
+export class Investor {
+    //params
+    isInvestor: bool;
+    shares: u128 = initialValue;
+
+    constructor(public name: string) {
+        this.isInvestor = true;
+    }
+
+    public updateShares(shares: u128, command: string): void {
+        if (command == "add") {
+            this.shares = u128.add(this.shares, shares);
+        } else if (command == "sub") {
+            this.shares = u128.sub(this.shares, shares);
+        }
+    }
+
+    public stillHasShares(): bool {
+        if (this.shares.toString() == initialValue.toString()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public revokeRights(): void {
+        this.isInvestor = false;
+    }
+}
+
+export const Investors = new PersistentUnorderedMap<string, Investor | null>("INVESTORS");
+
+export function checkIfInvestor(accountId: string): bool {
+    // get investor data
+    const investor = Investors.get(accountId);
+
+    // run checks
+    if (investor == null) {
+        return false
+    } else {
+        return investor.isInvestor
+    }
+}
+
+export const MinDeposit = u128.from(1000000000000000000000000);
